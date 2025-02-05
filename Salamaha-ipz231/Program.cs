b549de71e31c017ecbf62d5d1d1fcb758453d567
Passiv <ipz231_syao@student.ztu.edu.ua>
2025-02-05 20:06:13 +0200

    ''

---
+++ Salamaha-ipz231/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Salamaha_ipz231
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5; int b = 10;
+            Console.WriteLine(a+b);
+        }
+    }
+}
